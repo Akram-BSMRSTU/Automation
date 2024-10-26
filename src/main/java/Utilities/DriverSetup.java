73879f14d528cbe0090c80699a83c9c301499cf2
Akram Khan <akramkhanarafat12@gmail.com.com>
2024-10-27 00:26:07 +0600

    Setup Driver

--- src/main/java/Utilities/DriverSetup.java
+++ src/main/java/Utilities/DriverSetup.java
@@ -3,6 +3,8 @@ package Utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeSuite;
 
@@ -15,6 +17,22 @@ public class DriverSetup {
         driver = new FirefoxDriver();
         return driver;
 //
+
+    }
+
+    public WebDriver getbrowser(String browser_name){
+        if(browser_name.equalsIgnoreCase("Chrome")){
+            return new ChromeDriver() ;
+        } else if (browser_name.equalsIgnoreCase("Firefox")) {
+            return new FirefoxDriver();
+
+        }
+        else if (browser_name.equalsIgnoreCase("edge")){
+            return new EdgeDriver();
+        }
+        else {
+            throw new RuntimeException("Browser is not Available");
+        }
 
     }
 }
